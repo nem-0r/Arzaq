d58fb740e77be7845beab7a88cfa024db0e1d512
nem-0r <mirassarkytbek@gmail.com>
2025-12-08 03:46:57 +0500

    stable

---
+++ arzaq-backend/app/models/__init__.py
@@ -0,0 +1,15 @@
+# app/models/__init__.py
+from app.models.user import User
+from app.models.food import Food
+from app.models.order import Order, OrderItem
+from app.models.reservation import Reservation
+from app.models.payment import Payment
+
+__all__ = [
+    "User",
+    "Food",
+    "Order",
+    "OrderItem",
+    "Reservation",
+    "Payment",
+]
