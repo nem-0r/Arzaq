bf7e357a52586edcc6c8b446ebf21c4a80a9d944
Miras Sarkytbek <mirassarkytbek@gmail.com>
2025-12-07 22:58:41 +0500

    super last mb

--- arzaq-react/src/pages/CartPage/CartPage.jsx
+++ arzaq-react/src/pages/CartPage/CartPage.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useCart } from '../../context/CartContext';
-import { useAuth } from '../../context/AuthContext';
+import { useAuth } from '../../hooks/useAuth';
 import { useTranslation } from '../../hooks/useTranslation';
 import { formatPrice } from '../../utils/currency';
 import Header from '../../components/layout/Header/Header';
